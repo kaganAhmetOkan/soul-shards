6d63ce8c8e656414e86573fd81f70e6b6a2ca97f
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-08-08 00:15:13 +0300

    Added dynamic spell fetching

--- app/spell-schools/[school]/page.jsx
+++ app/spell-schools/[school]/page.jsx
@@ -1,12 +1,15 @@
 import { redirect } from "next/navigation";
 import Hero from "@/components/Hero/Hero";
+import fetchData from "@/utils/fetchData";
 
-export default function SchoolPage({ params }) {
+export default async function SchoolPage({ params }) {
   const schools = ["purple", "black", "white", "grey", "green", "red"];
-
+  
   if (!schools.includes(params.school)) {
     redirect("/spell-schools");
   };
+
+  const spells = await fetchData(`http://localhost:3000/spells/${params.school}.json`);
 
   return (
     <main>
