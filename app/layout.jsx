aff9244988ad56956e82a8ca7b40d63fec5198f6
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-08-07 16:14:53 +0300

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import './globals.css';
+import { Ubuntu } from 'next/font/google';
+
+const ubuntu = Ubuntu({
+  subsets: ['latin'],
+  weight: "400",
+  display: "swap",
+});
+
+export const metadata = {
+  title: 'Soul Shards',
+  description: 'Explore the definitive encyclopedia for the Soul Shards TTRPG system.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={ubuntu.className}>
+        {children}
+      </body>
+    </html>
+  );
+};
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import style from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <h1 className={style.hero}>Soul Shards</h1>
+    </main>
+  );
+};
