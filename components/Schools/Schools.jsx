15e484f77b5837c3a4fbfc5abc9a6ce1aabdc31c
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-08-09 13:47:43 +0300

    Improved Schools component

--- components/Schools/Schools.jsx
+++ components/Schools/Schools.jsx
@@ -1,44 +1,23 @@
 import style from "./Schools.module.css";
 import School from "../School/School";
-import iconPurple from "@/public/purple.png";
-import iconBlack from "@/public/black.png";
-import iconGreen from "@/public/green.png";
-import iconGrey from "@/public/grey.png";
-import iconRed from "@/public/red.png";
-import iconWhite from "@/public/white.png";
-
 
 export default function Schools() {
-  const icons = [
-    {
-      name: "purple",
-      img: iconPurple,
-    },
-    {
-      name: "black",
-      img: iconBlack,
-    },
-    {
-      name: "green",
-      img: iconGreen,
-    },
-    {
-      name: "grey",
-      img: iconGrey,
-    },
-    {
-      name: "red",
-      img: iconRed,
-    },
-    {
-      name: "white",
-      img: iconWhite,
-    },    
-  ];
-  
+  const shards = ["purple", "black", "green", "white", "red", "grey"]
+
   return (
     <div className={style.main}>
-      {icons.map(icon => <School icon={icon.img} key={icon.name} href={`/spell-schools/${icon.name}`} name={icon.name}>{icon.name}</School>)}
+      {shards.map(shard => {
+        return (
+          <School 
+            icon={`/${shard}.png`} 
+            key={shard} 
+            href={`/spell-schools/${shard}`} 
+            name={shard}
+          >
+            {shard}
+          </School>
+        )
+      })}
     </div>
   );
 };
